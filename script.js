1672dcb8ee60e044a177608c331abb81df78d084
Oksentiy <94550267+Oksentiy@users.noreply.github.com>
2021-12-29 19:49:50 +0200

    create buttoms

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 "use srtict";
+const btnRock = document.getElementById("rock");
+const btnScissors = document.getElementById("scissors");
+const btnPaper = document.getElementById("paper");
+const choiceRock = document.createElement("img");
 
-//
+btnRock.addEventListener("click", () => {
+  document.getElementById("my-container").innerHTML =
+    "<img src='images/rock.png' class=\"my-container\"  /> ";
+});
+
+btnScissors.addEventListener("click", () => {
+  document.getElementById("my-container").innerHTML =
+    "<img src='images/scissors.png' class=\"my-container\"  /> ";
+});
+
+btnPaper.addEventListener("click", () => {
+  document.getElementById("my-container").innerHTML =
+    "<img src='images/paper.png' class=\"my-container\"  /> ";
+});
